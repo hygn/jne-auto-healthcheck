ba04784fcd092d402b6e9b407de0e24ab190debf
Si-yeong Jang <proton_hygn@protonmail.com>
2020-06-25 22:37:53 +0000

    Exception handling

--- schedule-run.py
+++ schedule-run.py
@@ -6,10 +6,14 @@ def job():
     f = open('credentials.csv', 'r')
     credentials = f.read().split('\n')
     for i in credentials:
-        udata = i.split(',')
-        catcher = sender(udata[0], udata[1], udata[2])
-        today = date.today()
-        print(catcher + " " + str(today))
+        try:
+            udata = i.split(',')
+            catcher = sender(udata[0], udata[1], udata[2])
+            today = date.today()
+            print(catcher + " " + str(today)
+        except
+            print('index out of range'
+            pass
 schedule.every().day.at("07:30").do(job)
 while True:
     schedule.run_pending()
