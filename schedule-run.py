73d03238cfdadee481291808ee00dfe04af0ad16
Si-yeong Jang <proton_hygn@protonmail.com>
2020-06-25 22:46:33 +0000

    Update schedule-run.py

--- schedule-run.py
+++ schedule-run.py
@@ -1,7 +1,8 @@
 from healthcheck import sender
 import schedule
 from datetime import date
-import time
+import time 
+import sys
 def job():
     f = open('credentials.csv', 'r')
     credentials = f.read().split('\n')
@@ -11,11 +12,16 @@ def job():
             catcher = sender(udata[0], udata[1], udata[2])
             today = date.today()
             print(catcher + " " + str(today)
-        except
+        except:
             print('exception occured')
             pass
-schedule.every().day.at("07:30").do(job)
-while True:
-    schedule.run_pending()
-    time.sleep(30)
+if sys.argv[1] == 'schedule':
+    schedule.every().day.at("07:30").do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(30)
+elif sys.argv[1] == 'run':
+    job()
+else:
+    pass
     
